e5e10c1e12ed0f99f45d9b38d68fa12725399b75
juan-gil-05 <gilj06@hotmail.com>
2026-06-05 14:49:37 +0200

    feat: register vehicle_api models in admin site

--- backend/vehicle_api/admin.py
+++ backend/vehicle_api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    Status,
+    Fuel,
+    Gearbox,
+    ContractType,
+    Make,
+    Model,
+    Vehicle,
+    LeaseDetail,
+    Image,
+)
 
 # Register your models here.
+admin.site.register(Status)
+admin.site.register(Fuel)
+admin.site.register(Gearbox)
+admin.site.register(ContractType)
+admin.site.register(Make)
+admin.site.register(Model)
+admin.site.register(Vehicle)
+admin.site.register(LeaseDetail)
+admin.site.register(Image)
